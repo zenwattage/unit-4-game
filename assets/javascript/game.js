d799a2a6295217116976404a7f48f09a9604aec9
Scott H <zenwattage@gmail.com>
2019-03-17 21:45:32 -0700

    update to logic/display

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,114 +1,108 @@
 $(document).ready(function () {
 
-    //var redCrystal = $("<img>");
-
     console.log("this bitch is ready");
 
-    var dealerHand;
-    var wins;
-    var losses;
-    var totalScore;
-    var runningTotal;
+    //display scoreboard
+    //wins and losses
+    
+
+
+
+    var dealerHand = 0;
+    var wins = 0;
+    var losses = 0;
+    var totalScore = 0;
+    var runningTotal = 0;
     var red = 0;
-   // console.log(red);
     var green = 0;
-    //console.log(green);
     var blue = 0;
     var yellow = 0;
-    var cystalRandom;
-
+    
+    $("#scoreboard").append("Wins: " + wins);
+    $("#scoreboard").append("Losses: " + losses);
    //array of crystal variables
    var crystals = [red,green,blue,yellow];
-    //on game start 
-
-//BOOLEAN 
-//IF TRUE - USE HIGH
-// IF FALSE - USE LOW
   
-//generate random number for dealer between 19 and 120
+  //generate random number for dealer between 19 and 120
     function getRandomDealer() {
       var dealerMax = 120;
       var dealerMin = 19;
       return Math.floor(Math.random() * (dealerMax - dealerMin + 1)) + dealerMin;
-
-    }
-
+    };
 
     function getRandomCrystal() {
       var crystalMax = 12;
       var crystalMin = 1;
       return Math.floor(Math.random() * (crystalMax - crystalMin + 1)) + crystalMin;
+    };
 
-    }
-
-    //map to 
-    var newCrystalArray = crystals.map(crystals => getRandomCrystal());
-
-    console.log(newCrystalArray);
-
-    /* 
+    // generate new random number for dealerHand
+    // reset totalScore to 0
+    // assign new random number to each crystal
     function resetStuff() {
-      //for each index of crystals
-      //getRandomCrystal();
-      // crystals[i] = crystalRandom;
-    } */
-    /*
-    crystals.forEach(function(elem) {
-      crystals = getRandomCrystal();
+      runningTotal = 0;
+      dealerHand = getRandomDealer();
+      newCrystalArray = crystals.map(crystals => getRandomCrystal());
 
-    });*/
+    };
+
+    //map random crystal number to each crystal
+    var newCrystalArray = crystals.map(crystals => getRandomCrystal());
     
-    
-    
-    //for each crystal 
-     //run function on crystal random
-     // assign crystal random to crystal
+    console.log("Array is: " + newCrystalArray);
     
 
+    //display dealer hand
+    dealerHand = getRandomDealer();
+    $("#dealer_hand").append(dealerHand);
+    
+    console.log("Dealer hand is: " + dealerHand);
 
-
-    //console.log(dealerHand);
-    //console.log(crystalRandom);
-
-    //displayToUser dealerHand
-    //$("#dealer_hand").text(dealerHand);
-
-    //display 4 crystals
-    /* $("#red").prepend('<img src="assets/images/red.jpg" />')
-    $("#green").prepend('<img src="assets/images/green.jpg" />')
-    $("#blue").prepend('<img src="assets/images/blue.jpg" />')
-    $("#yellow").prepend('<img src="assets/images/yellow.jpg" />') */
-      // red, green, yellow, blue
-
-
-  // each of the 4 crystals gets a random number between 1 and 12
-  
-
+    
+      
     //on crystal click
-      // add this crystals worth to totalScore
-      // displayToUser totalScore
+    $("#red").on("click", function() {
+      runningTotal += newCrystalArray[0];      
+      console.log("Running total is: " + runningTotal);
+      $("#total_score").text(runningTotal);
+    });
+    $("#green").on("click", function() {
+      runningTotal += newCrystalArray[1];
+      console.log("Running total is: " + runningTotal);
+      $("#total_score").text(runningTotal);
+    });
+    $("#blue").on("click", function() {
+      runningTotal += newCrystalArray[2];
+      console.log("Running total is: " + runningTotal);
+      $("#total_score").text(runningTotal);
+    });
+    $("#yellow").on("click", function() {
+      runningTotal += newCrystalArray[3];
+      console.log(runningTotal);
+      $("#total_score").text(runningTotal);
+      console.log("Running total is: " + runningTotal);
+    });
+ 
+
+    if(runningTotal === dealerHand) {
+      
+      $("#scoreboard").append("<p>You won!!</p>");
+      wins++;
+      $("#scoreboard").append("Wins: " + wins);
+      resetStuff();
+    };
 
     
-    //if totalScore === dealerHand
-      // display "You won" to user
-      // add 1 to wins counter
-      
-      //
-      // generate new random number for dealerHand
-      // reset totalScore to 0
-      // assign new random number to each crystal
-
     //if totalScore > dealerHand
-      // displayToUser "You lost!!"
+    // displayToUser "You lost!!"
       // add 1 to Losses
-      
-      // 
-      // generate new random number for dealerHand
-      // reset totalScore to 0
-      // assign new random number to each crystal
+    if(runningTotal > dealerHand){
+      $("#scoreboard").append("You lost!!");
+      losses++;
+      $("#scoreboard").append("Losses: " + losses);
+      resetStuff();
+    };
 
-
-
-  });
+   });
 
 
